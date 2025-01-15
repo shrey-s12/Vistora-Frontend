5e7986f4450837fdcf919887f151a6c76d37f4d3
Shrey Singhal <shreynbd12@gmail.com>
2025-01-15 12:22:37 +0530

    Shopping Cart Page

--- src/pages/ShoppingCardPage.jsx
+++ src/pages/ShoppingCardPage.jsx
@@ -4,7 +4,7 @@ import ShoppingCard from '../components/shoppingCart/shoppingCard'
 const ShoppingCardPage = () => {
   return (
     <div className='bg-gray-200 w-full h-screen'>
-      <div className='bg-white'>
+      <div className='flex'>
         <ShoppingCard />
       </div>
     </div>
