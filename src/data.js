a06122dc5c8f033f160100f2618c0a129ad2c002
Shrey Singhal <shreynbd12@gmail.com>
2025-01-19 02:55:48 +0530

    Static Profile view data

--- src/data.js
+++ src/data.js
@@ -1,6 +1,13 @@
 import Heater1 from "./assets/Heater1.jpg";
 import Heater2 from "./assets/Heater2.jpg";
 
+import OrderBox from "./assets/orderBox.png"
+import Lock from "./assets/Lock.png"
+import PrimeBox from "./assets/Prime.png"
+import Location from "./assets/Location.png"
+import AmazonBusiness from "./assets/AmazonBusiness.jpg"
+
+
 export const products = [
     {
         id: 1,
@@ -24,4 +31,37 @@ export const products = [
         style: "Aquad Pro",
         quantity: 1,
     },
+]
+
+export const profile_view = [
+    {
+        id: crypto.randomUUID(),
+        img: OrderBox,
+        title: "Your Orders",
+        description: "Track, return, or buy things again",
+    },
+    {
+        id: crypto.randomUUID(),
+        img: Lock,
+        title: "Login & Security",
+        description: "Edit login, name, and mobile number",
+    },
+    {
+        id: crypto.randomUUID(),
+        img: PrimeBox,
+        title: "Prime",
+        description: "View benefits and payment settings",
+    },
+    {
+        id: crypto.randomUUID(),
+        img: Location,
+        title: "Your Addresses",
+        description: "Edit addresses for orders and gifts",
+    },
+    {
+        id: crypto.randomUUID(),
+        img: AmazonBusiness,
+        title: "Your business account",
+        description: "Sign up for free to save up to 28% with GST invoice and bulk discounts and purchase on credit.",
+    }
 ]
