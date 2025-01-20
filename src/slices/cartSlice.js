78fcd85696d4ae906a9007a1aba8656699d0df96
Shrey Singhal <shreynbd12@gmail.com>
2025-01-21 00:45:28 +0530

    CartSlice for addItem, removeItem, toggleItem and changeQuantity

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -1,20 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { products } from '../data';
 
 const initialState = {
-    items: products.slice(0, 2).map(ele => ({ ...ele, quantity: 1, selected: true }))
+    items: [],
 }
 
 export const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        setCartItems: (state, action) => {
-            state.items = action.payload;
-        },
         addItem: (state, action) => {
             const item = action.payload;
-            const itemInCart = state.items.find(ele => item.id === ele.id);
+            console.log("item", item);
+            const itemInCart = state.items.find(ele => item._id === ele._id);
+            console.log("itemInCart", itemInCart);
             if (itemInCart) return;
             state.items.push({
                 ...item,
@@ -22,26 +20,30 @@ export const cartSlice = createSlice({
                 selected: true,
             });
         },
+        removeItem: (state, action) => {
+            const id = action.payload;
+            state.items = state.items.filter(ele => ele._id !== id);
+        },
         toggleItem: (state, action) => {
             const id = action.payload;
-            const item = state.items.find(item => item.id === id);
+            const item = state.items.find(item => item._id === id);
             if (item) {
                 item.selected = !item.selected;
             }
         },
         changeQuantity: (state, action) => {
             const { id, increament } = action.payload;
-            const item = state.items.find(item => item.id === id);
+            const item = state.items.find(item => item._id === id);
             if (item) {
                 item.quantity += increament;
                 if (item.quantity === 0) {
-                    state.items = state.items.filter(item => item.id !== id);
+                    state.items = state.items.filter(item => item._id !== id);
                 }
             }
         }
     }
 });
 
-export const { addItem, toggleItem, changeQuantity } = cartSlice.actions;
+export const { addItem, removeItem, toggleItem, changeQuantity } = cartSlice.actions;
 
 export default cartSlice.reducer;
