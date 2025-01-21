d2e0d1d20846db4b432c587b3acf6b4f8736e9bd
Shrey Singhal <shreynbd12@gmail.com>
2025-01-21 16:19:50 +0530

    SideMedu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HiOutlineBars3 } from "react-icons/hi2";
 import { IoMdArrowDropdown } from "react-icons/io";
 import HeaderImage from "../../assets/header-image.jpg";
 import { headerProducts } from "../../data";
+import SideMenu from './SideMenu';
 
 const Product = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
     return (
         <div className='flex items-center w-[80%]'>
-            <div className='flex ml-4 p-2 hover:outline hover:outline-1 hover:outline-white'>
+            <div
+                className='flex ml-4 p-2 hover:outline hover:outline-1 hover:outline-white'
+                onClick={toggleMenu}
+            >
                 <HiOutlineBars3 className='text-2xl' />
-                <span>All</span>
+                <button>All</button>
             </div>
 
+            <SideMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} />
+
             {headerProducts.map((product, index) => (
-                <span key={index} className='p-2 hover:outline hover:outline-1 hover:outline-white'>{product.name}</span>
+                <button key={index} className='p-2 hover:outline hover:outline-1 hover:outline-white'>{product.name}</button>
             ))}
 
             <div className='flex p-2 hover:outline hover:outline-1 hover:outline-white'>
-                <span>Browsing History</span>
+                <button>Browsing History</button>
                 <IoMdArrowDropdown className='text-2xl' />
             </div>
-
-
         </div>
     )
 }
---
+++ src/components/header/SideMenu.jsx
@@ -0,0 +1,137 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import { setCurrentUser } from "../../slices/authSlice";
+
+const SideMenu = ({ isOpen, toggleMenu }) => {
+    const username = useSelector((state) => state.auth.currentUser);
+    console.log(username, "username");
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get('http://localhost:5000/user/userInfo', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(response => {
+                console.log(response.data);
+                dispatch(setCurrentUser({ email: response.data.user.username }));
+            })
+            .catch(err => console.error(err));
+    }, [username]);
+
+
+    return (
+        <>
+            <div
+                className={`fixed top-0 left-0 h-full w-80 bg-gray-800 text-white transform ${isOpen ? "translate-x-0" : "-translate-x-full"
+                    } transition-transform duration-300 ease-in-out z-50 overflow-y-auto`}
+            >
+
+                <div className="bg-gray-800 flex items-center text-white px-5 py-3">
+                    <span className="material-icons mr-1">account_circle</span>
+                    <h2 className="text-lg font-bold mt-2">{username ? `Hello, ${username}` : "Hello, Customer"}</h2>
+                </div>
+
+
+                <div className="bg-white text-black px-6 py-4 space-y-6">
+                    <div>
+                        <h3 className="font-bold text-black mb-2">Trending</h3>
+                        <ul className="space-y-2">
+                            <li className="cursor-pointer hover:underline">Best Sellers</li>
+                            <li className="cursor-pointer hover:underline">New Releases</li>
+                            <li className="cursor-pointer hover:underline">
+                                Movers and Shakers
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3 className="font-bold text-black mb-2">
+                            Digital Content and Devices
+                        </h3>
+                        <ul className="space-y-2">
+                            <li className="cursor-pointer hover:underline">Echo & Alexa</li>
+                            <li className="cursor-pointer hover:underline">Fire TV</li>
+                            <li className="cursor-pointer hover:underline">
+                                Kindle E-Readers & eBooks
+                            </li>
+                            <li className="cursor-pointer hover:underline">
+                                Audible Audiobooks
+                            </li>
+                            <li className="cursor-pointer hover:underline">
+                                Amazon Prime Video
+                            </li>
+                            <li className="cursor-pointer hover:underline">
+                                Amazon Prime Music
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3 className="font-bold text-black mb-2">Shop by Category</h3>
+                        <ul className="space-y-2">
+                            <li className="cursor-pointer hover:underline">
+                                Mobiles, Computers
+                            </li>
+                            <li className="cursor-pointer hover:underline">
+                                TV, Appliances, Electronics
+                            </li>
+                            <li className="cursor-pointer hover:underline">Men's Fashion</li>
+                            <li className="cursor-pointer hover:underline">Women's Fashion</li>
+                            <li className="cursor-pointer hover:underline">See all</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3 className="font-bold text-black mb-2">Programs & Features</h3>
+                        <ul className="space-y-2">
+                            <li className="cursor-pointer hover:underline">Amazon Pay</li>
+                            <li className="cursor-pointer hover:underline">
+                                Gift Cards & Mobile Recharges
+                            </li>
+                            <li className="cursor-pointer hover:underline">
+                                Amazon Launchpad
+                            </li>
+                            <li className="cursor-pointer hover:underline">Amazon Business</li>
+                            <li className="cursor-pointer hover:underline">See all</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3 className="font-bold text-black mb-2">Help & Settings</h3>
+                        <ul className="space-y-2">
+                            <li className="cursor-pointer hover:underline">Your Account</li>
+                            <li className="cursor-pointer hover:underline">
+                                Customer Service
+                            </li>
+                            <li className="cursor-pointer hover:underline">Sign in</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            {isOpen && (
+                <div className="fixed top-4 left-80 z-50">
+                    <span
+                        className="material-icons cursor-pointer bg-transparent text-white border-2 border-white rounded-md p-2 ml-1"
+                        onClick={toggleMenu}
+                    >
+                        close
+                    </span>
+                </div>
+            )}
+
+            {isOpen && (
+                <div
+                    className="fixed inset-0 bg-black opacity-50 z-40"
+                    onClick={toggleMenu}
+                ></div>
+            )}
+        </>
+    );
+};
+
+export default SideMenu;
